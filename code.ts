28fb8a940b755ad1c2900fb73cea2d533d2706a3
Uthirakumar123 <117447508+Uthirakumar123@users.noreply.github.com>
2023-12-22 16:21:56 +0530

    2update

--- code.ts
+++ code.ts
@@ -59,100 +59,137 @@
 //   figma.closePlugin();
 // };
 
-figma.showUI(__html__,{ width: 400, height: 400 });
+// figma.showUI(__html__,{ width: 400, height: 400 });
+
+// figma.ui.onmessage = msg => {
+//   if (msg.type === 'show-frame-details') {
+//     const selectedFrame = figma.currentPage.selection[0];
+
+//     if (selectedFrame && selectedFrame.type === 'FRAME') {
+//       const frameDetails = {
+//         name: selectedFrame.name,
+//         width: selectedFrame.width,
+//         height: selectedFrame.height,
+//         children: selectedFrame.children.map(child => ({ name: child.name, type: child.type })),
+//       };
+
+//       // Send frame details to the UI
+//       figma.ui.postMessage({ type: 'update-frame-details', frameDetails });
+//     }
+//   }
+
+//   if (msg.type === 'edit-frame') {
+//     const selectedFrame = figma.currentPage.selection[0];
+
+//     if (selectedFrame && selectedFrame.type === 'FRAME') {
+//       // Show the properties panel for the selected frame
+//       figma.viewport.scrollAndZoomIntoView([selectedFrame]);
+//       figma.currentPage.selection = [selectedFrame];
+//     }
+//   }
+
+//   if (msg.type === 'duplicate-frame') {
+//     const selectedFrame = figma.currentPage.selection[0];
+
+//     if (selectedFrame && selectedFrame.type === 'FRAME') {
+//       // Duplicate the selected frame and its children
+//       const duplicatedFrame = duplicateFrameWithChildren(selectedFrame);
+
+//       figma.currentPage.appendChild(duplicatedFrame);
+//       figma.currentPage.selection = [duplicatedFrame];
+//     }
+//   }
+
+//   if (msg.type === 'duplicate-child') {
+//     const selectedFrame = figma.currentPage.selection[0];
+
+//     if (selectedFrame && selectedFrame.type === 'FRAME') {
+//       const childToDuplicate = selectedFrame.children.find(child => child.name === msg.childName);
+
+//       if (childToDuplicate) {
+//         const duplicatedChild = childToDuplicate.clone();
+//         selectedFrame.appendChild(duplicatedChild);
+//       }
+//     }
+//   }
+  
+
+//   if (msg.type === 'cancel') {
+//     figma.closePlugin();
+//   }
+// };
+
+// function duplicateFrameWithChildren(frame:any) {
+//   // Duplicate the frame
+//   const duplicatedFrame = frame.clone();
+//   duplicatedFrame.x += frame.width + 100; // Adjust the x position for the duplicated frame
+//   duplicatedFrame.y += frame.height + 100; // Adjust the y position for the duplicated frame
+
+//   // Clear existing children in the duplicated frame
+//   duplicatedFrame.children = [];
+
+//   // Duplicate and append children to the duplicated frame
+//   frame.children.forEach((child: { clone: () => any; }) => {
+//     const duplicatedChild = child.clone();
+//     duplicatedFrame.appendChild(duplicatedChild);
+//   });
+
+//   return duplicatedFrame;
+// } 
+
+figma.showUI(__html__, { width: 240, height: 180 });
 
 figma.ui.onmessage = msg => {
-  if (msg.type === 'show-frame-details') {
-    const selectedFrame = figma.currentPage.selection[0];
-
-    if (selectedFrame && selectedFrame.type === 'FRAME') {
-      const frameDetails = {
-        name: selectedFrame.name,
-        width: selectedFrame.width,
-        height: selectedFrame.height,
-        children: selectedFrame.children.map(child => ({ name: child.name, type: child.type })),
-      };
-
-      // Send frame details to the UI
-      figma.ui.postMessage({ type: 'update-frame-details', frameDetails });
-    }
-  }
-
-  if (msg.type === 'edit-frame') {
-    const selectedFrame = figma.currentPage.selection[0];
-
-    if (selectedFrame && selectedFrame.type === 'FRAME') {
-      // Show the properties panel for the selected frame
-      figma.viewport.scrollAndZoomIntoView([selectedFrame]);
-      figma.currentPage.selection = [selectedFrame];
-    }
-  }
-
   if (msg.type === 'duplicate-frame') {
     const selectedFrame = figma.currentPage.selection[0];
 
     if (selectedFrame && selectedFrame.type === 'FRAME') {
-      // Duplicate the selected frame and its children
-      const duplicatedFrame = duplicateFrameWithChildren(selectedFrame);
+      // Get the target screen size
+      const screenSize = msg.screenSize;
+
+      // Duplicate the selected frame and adjust the size and position based on the screen size
+      const duplicatedFrame = selectedFrame.clone();
+      adjustFrameForScreenSize(duplicatedFrame, screenSize);
 
       figma.currentPage.appendChild(duplicatedFrame);
       figma.currentPage.selection = [duplicatedFrame];
     }
   }
 
-  if (msg.type === 'duplicate-child') {
-    const selectedFrame = figma.currentPage.selection[0];
-
-    if (selectedFrame && selectedFrame.type === 'FRAME') {
-      const childToDuplicate = selectedFrame.children.find(child => child.name === msg.childName);
-
-      if (childToDuplicate) {
-        const duplicatedChild = childToDuplicate.clone();
-        selectedFrame.appendChild(duplicatedChild);
-      }
-    }
-  }
-  
-
   if (msg.type === 'cancel') {
     figma.closePlugin();
   }
 };
 
-function duplicateFrameWithChildren(frame:any) {
-  // Duplicate the frame
-  const duplicatedFrame = frame.clone();
-  duplicatedFrame.x += frame.width + 100; // Adjust the x position for the duplicated frame
-  duplicatedFrame.y += frame.height + 100; // Adjust the y position for the duplicated frame
+// function adjustFrameForScreenSize(frame: any, screenSize: string) {
+//   // Define your size adjustments based on screen size
+//   const sizeAdjustments = {
+//     mobile: { width: 320, height: 480 },
+//     tablet: { width: 768, height: 1024 },
+//     desktop: { width: 1366, height: 768 },
+//   };
 
-  // Clear existing children in the duplicated frame
-  duplicatedFrame.children = [];
+//   // Apply adjustments
+//   frame.resize(sizeAdjustments[screenSize].width, sizeAdjustments[screenSize].height);
+//   // You can also adjust the frame position if needed
+//   frame.x += 100;
+//   frame.y += 100;
+// }
 
-  // Duplicate and append children to the duplicated frame
-  frame.children.forEach((child: { clone: () => any; }) => {
-    const duplicatedChild = child.clone();
-    duplicatedFrame.appendChild(duplicatedChild);
-  });
+function adjustFrameForScreenSize(frame: FrameNode, screenSize: string) {
+  // Define your size adjustments based on screen size
+  const sizeAdjustments = {
+    mobile: { width: 320, height: 480 },
+    tablet: { width: 768, height: 1024 },
+    desktop: { width: 1366, height: 768 },
+  };
 
-  return duplicatedFrame;
+  const newSize = sizeAdjustments[screenSize];
+
+  // Resize the frame
+  frame.resize(newSize.width, newSize.height);
+
+  // Adjust the position if needed
+  frame.x += 100;
+  frame.y += 100;
 }
-
-
-// async function customizeDuplicatedFrame(duplicatedFrame: FrameNode) {
-//     await figma.loadFontAsync({ family: "Inter", style: "Regular" });
-
-//     const textNode = figma.createText();
-//     textNode.characters = 'Hello, Figma!';
-//     textNode.x = 20;
-//     textNode.y = 20;
-
-//     const rectangleNode = figma.createRectangle();
-//     rectangleNode.resize(100, 100);
-//     rectangleNode.x = 20;
-//     rectangleNode.y = 100;
-//     rectangleNode.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 1 } }];
-
-//     duplicatedFrame.appendChild(textNode);
-//     duplicatedFrame.appendChild(rectangleNode);
-//   }
-  
